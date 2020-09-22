deaa9c0d72a0923bb3e29ab98c0faca7ae4f061c
Dave Biddle <david.biddle@soak.co.uk>
2020-09-22 15:06:31 +0100

    Began markup in homepage components

--- src/components/PreviousQuotesBlock.js
+++ src/components/PreviousQuotesBlock.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function PreviousQuotesBlock() {
-  return <div>Previous Quotes Block</div>;
+  return (
+    <div>
+      <h2>Previous Quotes</h2>
+    </div>
+  );
 }
 
 export default PreviousQuotesBlock;
--- src/components/TodaysRandomQuote.js
+++ src/components/TodaysRandomQuote.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function TodaysRandomQuote() {
-  return <div>Today's Random Quote</div>;
+  return (
+    <div>
+      <h3>{new Date().toString()}</h3>
+      <h1>Today's Random Quote:</h1>
+    </div>
+  );
 }
 
 export default TodaysRandomQuote;
