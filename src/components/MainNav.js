74a36953af122e1c65f39d920b5ebd0c4b601040
Dave Biddle <david.biddle@soak.co.uk>
2020-09-23 11:32:33 +0100

    Added markup to MainNav component

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,7 +1,22 @@
 import React from "react";
+import logo from "img/logo-header-117x72.png";
 
 function MainNav() {
-  return <div>Main Nav</div>;
+  return (
+    <div>
+      <a href="/">
+        <img src={logo} alt="Logo" />
+      </a>
+      <ul>
+        <li>
+          <a href="/">Today's Quote</a>
+        </li>
+        <li>
+          <a href="/">Previous Quotes</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default MainNav;
