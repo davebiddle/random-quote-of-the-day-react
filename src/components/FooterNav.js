3f5ebe28e660d8e714e5deaf2f713364b1e35da5
Dave Biddle <david.biddle@soak.co.uk>
2020-09-23 13:25:36 +0100

    Added nav and logo to FooterNav component

--- src/components/FooterNav.js
+++ src/components/FooterNav.js
@@ -1,7 +1,29 @@
 import React from "react";
+import logo_footer from "img/logo-footer-140x86.png";
 
 function FooterNav() {
-  return <div>Footer Nav</div>;
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Today's Quote</a>
+          </li>
+          <li>
+            <a href="/">Previous Quotes</a>
+          </li>
+          <li>
+            <a href="/about">About Random QOTD</a>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <a href="/">
+          <img src={logo_footer} alt="Logo" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default FooterNav;
