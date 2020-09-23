593f9d46f69aae936c25936be1c4468515b769a4
Dave Biddle <david.biddle@soak.co.uk>
2020-09-23 06:58:39 +0100

    Added markup in TodaysRandomQuote block

--- src/components/TodaysRandomQuote.js
+++ src/components/TodaysRandomQuote.js
@@ -5,6 +5,34 @@ function TodaysRandomQuote() {
     <div>
       <h3>{new Date().toString()}</h3>
       <h1>Today's Random Quote:</h1>
+      <div>
+        <blockquote cite="https://quotepark.com/quotes/1830729-leonardo-da-vinci-the-variety-of-colour-in-objects-cannot-be-discern/">
+          The variety of colour in objects cannot be discerned at a great
+          distance, excepting in those parts which are directly lighted up by
+          the solar rays."
+        </blockquote>
+        <div>
+          - Leonardo da Vinci
+          <hr />
+          <ul>
+            <li>
+              <a href="https://quotepark.com/quotes/1830729-leonardo-da-vinci-the-variety-of-colour-in-objects-cannot-be-discern/">
+                View this quote on quotepark.com
+              </a>
+            </li>
+            <li>
+              <a href="https://quotepark.com/authors/leonardo-da-vinci/">
+                More quotes from Leonardo da Vinci
+              </a>
+            </li>
+            <li>
+              <a href="https://en.wikiquote.org/w/index.php?search=Leonardo%20da%20Vinci">
+                Find Leonardo da Vinci on Wikiquote
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
