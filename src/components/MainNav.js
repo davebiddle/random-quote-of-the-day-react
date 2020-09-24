1723d2ef449ae888f9740f36eddaf112fb83d3bb
Dave Biddle <david.biddle@soak.co.uk>
2020-09-24 11:05:38 +0100

    Added layout styling to MainNav

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -3,16 +3,17 @@ import logo from "img/logo-header-117x72.png";
 
 function MainNav() {
   return (
-    <div>
+    <div className="w-full h-24 pl-24 pr-8 flex justify-between items-center">
       <a href="/">
         <img src={logo} alt="Logo" />
       </a>
-      <ul>
+      <ul className="flex main-nav">
         <li>
           <a href="/">Today's Quote</a>
         </li>
+        <li>|</li>
         <li>
-          <a href="/">Previous Quotes</a>
+          <a href="/previous-quotes">Previous Quotes</a>
         </li>
       </ul>
     </div>
