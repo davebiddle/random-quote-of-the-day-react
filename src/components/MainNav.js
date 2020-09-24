238c3fc12c972ba3f753f8aae84cabcc426dabbf
Dave Biddle <david.biddle@soak.co.uk>
2020-09-24 15:21:50 +0100

    Added custom config with site color scheme colors

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -3,9 +3,9 @@ import logo from "img/logo-header-117x72.png";
 
 function MainNav() {
   return (
-    <div className="w-full h-24 pl-24 pr-8 flex justify-between items-center">
+    <div className="w-full h-24 pl-24 pr-8 flex justify-between items-center bg-genoa">
       <a href="/">
-        <img src={logo} alt="Logo" />
+        <img src={logo} alt="Logo main" />
       </a>
       <ul className="flex main-nav">
         <li>
---
+++ src/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        genoa: "#167070",
+        "faded-jade": "#46857d",
+        orange: "#fe6625",
+        "neon-carrot": "#fd9230",
+        "astronaut-blue": "#003d59",
+        mako: "#414a4f",
+        white: "#ffffff",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
