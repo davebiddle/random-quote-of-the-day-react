7776187aee01fde8efc2975e9836d83799f593ed
Dave Biddle <david.biddle@soak.co.uk>
2020-10-04 16:07:21 +0100

    Added styling for sm breakpoint

--- src/components/previous-quotes/PreviousQuotesListing.js
+++ src/components/previous-quotes/PreviousQuotesListing.js
@@ -74,11 +74,11 @@ function PreviousQuotesListing() {
 
   return (
     <div className="previous-quotes-listing">
-      <header className="px-4 py-2 text-gray-600 text-sm italic">
-        <p className="mb-2">Showing 1-20 of 1023 quotes</p>
+      <header className="px-4 sm:px-8 py-2 text-gray-600 text-sm italic sm:flex sm:justify-between sm:items-center">
+        <p className="mb-2 sm:mb-0">Showing 1-20 of 1023 quotes</p>
         <div>
           <label for="select-order">Order by:</label>
-          <div className="inline-block relative w-56 ml-4">
+          <div className="inline-block relative w-48 ml-4">
             <select
               id="select-order"
               name="select-order"
@@ -89,7 +89,7 @@ function PreviousQuotesListing() {
               </option>
               <option value="asc">Date (Oldest first)</option>
             </select>
-            <div className="pointer-events-none absolute inset-y-0 right-0 flex justify-center items-center border-l-2 border-gray-400 w-12">
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex justify-center items-center border-l-2 border-gray-400 w-10">
               <svg
                 width="3.2217mm"
                 height="1.933mm"
