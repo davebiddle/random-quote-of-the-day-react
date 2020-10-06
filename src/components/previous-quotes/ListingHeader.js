f8199760ea1c5855524479204b62c581c88d0ad6
Dave Biddle <david.biddle@soak.co.uk>
2020-10-06 05:06:11 +0100

    Added Quotes Context and consumed in relevant components

--- src/components/previous-quotes/ListingHeader.js
+++ src/components/previous-quotes/ListingHeader.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import QuotesContext from "contexts/QuotesContext";
 
 function ListingHeader() {
+  const { orderBy } = useContext(QuotesContext);
+
   return (
     <header className="px-4 sm:px-8 md:px-10 py-2 md:h-16 lg:h-20 text-gray-600 text-sm italic sm:flex sm:justify-between sm:items-center lg:items-end lg:pl-0 lg:pt-0 lg:pb-4">
       <p className="mb-2 sm:mb-0 md:mt-7">Showing 1-20 of 1023 quotes</p>
@@ -10,6 +13,7 @@ function ListingHeader() {
           <select
             id="select-order"
             name="select-order"
+            onChange={orderBy}
             className="appearance-none w-full bg-white border-2 border-gray-400 hover:border-gray-600 rounded px-4 py-1 pr-8 focus:outline-none focus:shadow-outline"
           >
             <option value="desc" selected>
