bd95cd0f78b0387063575ed811f67f645ae04885
Dave Biddle <david.biddle@soak.co.uk>
2020-10-06 14:13:40 +0100

    Cleaned up Quotes Context by removing obsoleted functions

--- src/contexts/QuotesContext.js
+++ src/contexts/QuotesContext.js
@@ -2,9 +2,6 @@ import { createContext } from "react";
 
 const QuotesContext = createContext({
   quotes: [],
-  orderBy: (event) => {},
-  getPage: (pageNum) => {},
-  toggleOpenState: (quote) => {},
 });
 
 export default QuotesContext;
