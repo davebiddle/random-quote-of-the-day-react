11eeaa8169ddd2afde0827412cc4d67da006320e
Dave Biddle <david.biddle@soak.co.uk>
2020-10-14 10:39:00 +0100

    Added .env with config param for default per-page setting

--- src/components/previous-quotes/ListingHeader.js
+++ src/components/previous-quotes/ListingHeader.js
@@ -8,82 +8,96 @@ function ListingHeader() {
   return (
     <header className="px-4 sm:px-8 md:px-10 py-2 md:h-16 lg:h-20 text-gray-600 text-sm italic sm:flex sm:justify-between sm:items-center lg:items-end lg:pl-0 lg:pt-0 lg:pb-4">
       <p className="mb-2 sm:mb-0 md:mt-7">Showing 1-20 of 1023 quotes</p>
-      <div>
-        <label for="select-per-page">Per page:</label>
-        <div className="inline-block relative w-24 ml-4">
-          <select
-            id="select-per-page"
-            name="select-order"
-            onChange={(event) => {
-              const params = { ...filterQuery, per_page: event.target.value };
+      <div className="md:flex md:justify-end">
+        <div className="pr-4 mb-2 md:mb-0">
+          <label for="select-per-page">Per page:</label>
+          <div className="inline-block relative w-24 ml-4">
+            <select
+              id="select-per-page"
+              name="select-per-page"
+              onChange={(event) => {
+                const params = { ...filterQuery, per_page: event.target.value };
 
-              fetchPreviousQuotesData(`api/quotes`, params, dispatch);
-            }}
-            className="appearance-none w-full bg-white border-2 border-gray-400 hover:border-gray-600 rounded px-4 py-1 pr-8 focus:outline-none focus:shadow-outline"
-          >
-            {[5, 10, 15, 20, 25, 30].map((perPage, index) => (
-              <option value={perPage}>{perPage}</option>
-            ))}
-          </select>
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex justify-center items-center border-l-2 border-gray-400 w-10">
-            <svg
-              width="3.2217mm"
-              height="1.933mm"
-              version="1.1"
-              viewBox="0 0 3.2217 1.933"
-              xmlns="http://www.w3.org/2000/svg"
+                fetchPreviousQuotesData(`api/quotes`, params, dispatch);
+              }}
+              className="appearance-none w-full bg-white border-2 border-gray-400 hover:border-gray-600 rounded px-4 py-1 pr-8 focus:outline-none focus:shadow-outline"
             >
-              <g
-                transform="translate(-98.931 -142.66)"
-                fill="none"
-                stroke="#979797"
-                stroke-linecap="round"
-                stroke-width=".64434"
+              {[5, 10, 15, 20, 25, 30].map((perPage, index) => {
+                const selected = parseInt(
+                  process.env.REACT_APP_DEFAULT_PER_PAGE
+                );
+
+                return (
+                  <option
+                    key={index}
+                    value={perPage}
+                    selected={perPage === selected}
+                  >
+                    {perPage}
+                  </option>
+                );
+              })}
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex justify-center items-center border-l-2 border-gray-400 w-10">
+              <svg
+                width="3.2217mm"
+                height="1.933mm"
+                version="1.1"
+                viewBox="0 0 3.2217 1.933"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <path d="m101.83 142.99-1.2887 1.2887" />
-                <path d="m99.253 142.99 1.2887 1.2887" />
-              </g>
-            </svg>
+                <g
+                  transform="translate(-98.931 -142.66)"
+                  fill="none"
+                  stroke="#979797"
+                  stroke-linecap="round"
+                  stroke-width=".64434"
+                >
+                  <path d="m101.83 142.99-1.2887 1.2887" />
+                  <path d="m99.253 142.99 1.2887 1.2887" />
+                </g>
+              </svg>
+            </div>
           </div>
         </div>
-      </div>
-      <div>
-        <label for="select-order">Order by:</label>
-        <div className="inline-block relative w-48 ml-4">
-          <select
-            id="select-order"
-            name="select-order"
-            onChange={(event) => {
-              const params = { ...filterQuery, order: event.target.value };
+        <div>
+          <label for="select-order">Order by:</label>
+          <div className="inline-block relative w-48 ml-4">
+            <select
+              id="select-order"
+              name="select-order"
+              onChange={(event) => {
+                const params = { ...filterQuery, order: event.target.value };
 
-              fetchPreviousQuotesData(`api/quotes`, params, dispatch);
-            }}
-            className="appearance-none w-full bg-white border-2 border-gray-400 hover:border-gray-600 rounded px-4 py-1 pr-8 focus:outline-none focus:shadow-outline"
-          >
-            <option value="desc" selected>
-              Date (Newest first)
-            </option>
-            <option value="asc">Date (Oldest first)</option>
-          </select>
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex justify-center items-center border-l-2 border-gray-400 w-10">
-            <svg
-              width="3.2217mm"
-              height="1.933mm"
-              version="1.1"
-              viewBox="0 0 3.2217 1.933"
-              xmlns="http://www.w3.org/2000/svg"
+                fetchPreviousQuotesData(`api/quotes`, params, dispatch);
+              }}
+              className="appearance-none w-full bg-white border-2 border-gray-400 hover:border-gray-600 rounded px-4 py-1 pr-8 focus:outline-none focus:shadow-outline"
             >
-              <g
-                transform="translate(-98.931 -142.66)"
-                fill="none"
-                stroke="#979797"
-                stroke-linecap="round"
-                stroke-width=".64434"
+              <option value="desc" selected>
+                Date (Newest first)
+              </option>
+              <option value="asc">Date (Oldest first)</option>
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex justify-center items-center border-l-2 border-gray-400 w-10">
+              <svg
+                width="3.2217mm"
+                height="1.933mm"
+                version="1.1"
+                viewBox="0 0 3.2217 1.933"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <path d="m101.83 142.99-1.2887 1.2887" />
-                <path d="m99.253 142.99 1.2887 1.2887" />
-              </g>
-            </svg>
+                <g
+                  transform="translate(-98.931 -142.66)"
+                  fill="none"
+                  stroke="#979797"
+                  stroke-linecap="round"
+                  stroke-width=".64434"
+                >
+                  <path d="m101.83 142.99-1.2887 1.2887" />
+                  <path d="m99.253 142.99 1.2887 1.2887" />
+                </g>
+              </svg>
+            </div>
           </div>
         </div>
       </div>
